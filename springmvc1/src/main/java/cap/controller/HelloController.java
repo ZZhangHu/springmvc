acd0f70e49fbb49c457b504b475445a8cfc02846
ZZhangHu <Zhang_Hu_Fighting@163.com>
2018-09-04 09:26:31 +0800

    在hellocontroller中加入了test2、3、4

--- springmvc1/src/main/java/cap/controller/HelloController.java
+++ springmvc1/src/main/java/cap/controller/HelloController.java
@@ -3,6 +3,11 @@ package cap.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/")
 public class HelloController {
@@ -10,5 +15,22 @@ public class HelloController {
     public String test(Model model){
         model.addAttribute("data","SpringMVC");
         return "test";
+    }
+    @RequestMapping("/test2")
+    public void test2(Model model){
+        model.addAttribute("data","SpringMVC Test2");
+    }
+    @RequestMapping("/test3")
+    public ModelAndView test3(){
+        ModelAndView modelAndView=new ModelAndView("test3");
+        modelAndView.addObject("data","SpringMVC Test3");
+        return modelAndView;
+    }
+    @RequestMapping("/test4")
+    public Map test4(){
+        Map<String,String> map=new HashMap<String, String>();
+        map.put("key1","value1");
+        map.put("key2","value2");
+        return map;
     }
 }
